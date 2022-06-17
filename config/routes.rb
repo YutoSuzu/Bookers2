c58ad3f9a3b8ebeed2667f3e04b559fe885a5ee8
YutoSuzu <104748069+YutoSuzu@users.noreply.github.com>
2022-06-17 14:47:42 +0000

    [add]アソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  root to: "homes#top"
+  devise_for :users
+  resources :books, only: [:show, :index, :edit, :create, :destory, :update]
+  resources :user, only: [:show, :index, :edit, :u]
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
